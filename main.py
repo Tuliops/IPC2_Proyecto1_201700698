34d2ffcd60d64e8671dd6e303c8cc887f97c90e7
Tuliops <Tuliopirir3@gmail.com>
2022-02-21 19:25:02 -0600

    Lectrua

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+#-------import-------
+import xml.etree.ElementTree as ET
 
+
+#----------------------
 def MenuPrincipal(): 
     while True:
         # Mostramos el menu
@@ -38,13 +42,66 @@ def menu1():
     print ("\t4 - Seleccionar un nuevo código de patrón")
     print ("\t5  - salir")
         #Menu Principal del Programaa
+xpath = ('./2.xml')
+
+def elementTree(ruta):
+    tree = ET.parse(ruta)
+    raiz = tree.getroot()
+
+
+    """print('Atributos')
+    for element in raiz:
+        for subelement in element:
+            print(subelement.attrib)
+            print(subelement.text)
+        
+    print('Valores')
+    for element in raiz:
+        for subelement in element:
+            print(subelement.tag)
+            print(subelement.text)"""
+            
+
+
+
+    #Leer los Nombres de los Pisos 
+    for r in raiz:
+        nombre = r.text.replace('\n','')
+        print(r.attrib['nombre'],nombre)
+        print("--Matriz-----")
+        for sub in r:
+            R = ''
+            C = ''
+            F = ''
+            S = ''
+            tag = sub.tag 
+            if tag =='R': 
+                tag = tag.replace('\ ','')
+                R = sub.text
+                print("R = ",R)
+            elif tag =='C': 
+                tag = tag.replace('\ ','')
+                C = sub.text
+                print("C = ",C)
+            elif tag =='F': 
+                tag = tag.replace('\ ','')
+                F = sub.text
+                print("F = ",F)
+            elif tag =='S': 
+                tag = tag.replace('\ ','')
+                S = sub.text
+                print("S = ",S)
+            for p in sub :
+                
+                Codigo = p.text.replace('\n', '')
+                nombreCodigo = p.attrib['codigo']
+                
+               
 
 
 
 
-
-
-
-
+                print('\t','\t Nombre de Codigo ',nombreCodigo,'\n', '\t\tCodigo --->',Codigo)
 if __name__ == "__main__":
-   MenuPrincipal()
+    print(xpath)
+    elementTree(xpath)
